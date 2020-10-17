1fc42897a2cf0fccdcf04356c8313a8bc451f759
GooseMagnet <ilankarasik@gmail.com>
2020-10-17 01:13:47 -0400

    Finished Chapter 2 - Observer

--- Chapter02/src/main/java/com/goosemagnet/observer/swingobserver/SwingObserverExample.java
+++ Chapter02/src/main/java/com/goosemagnet/observer/swingobserver/SwingObserverExample.java
@@ -2,8 +2,6 @@ package com.goosemagnet.observer.swingobserver;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SwingObserverExample {
 
